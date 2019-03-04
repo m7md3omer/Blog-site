6521451b9c364d86934194e765af27c5655d78b1
m7md3omer <mohammedomer789@gmail.com>
2019-03-04 02:16:48 +0200

    added some styles with bootstrap and css and dummy data to display on the home page

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+app_name = 'blog'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
