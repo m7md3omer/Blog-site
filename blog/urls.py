fcdfa0f41a0a4f456ef10e4e4e8e66cba95890f8
m7md3omer <mohammedomer789@gmail.com>
2019-03-08 02:59:46 +0200

    using class based views instead of functions , also added the functionality of creating , updating and deleting and assign the privileges to users

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from . import views
+from .views import (PostListView,
+                    PostCreateView,
+                    PostDetailView,
+                    PostUpdateView,
+                    PostDeleteView,
+                    about)
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('', PostListView.as_view(), name='home'),
+    # pk is the default argument for primary keys so we don't have to tell the class about which instance to display
+    path('post/<int:pk>/', PostDetailView.as_view(), name='detail'),
+    path('post/new/', PostCreateView.as_view(), name='create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('about/', about, name='about'),
 ]
